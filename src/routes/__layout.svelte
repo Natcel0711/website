bc3748ae85b59e0e8e3c5f12db3c4430d0a5c8b9
natcel.nieves@upr.edu <natcel.nieves@upr.edu>
2022-06-19 10:40:25 -0400

    First Commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../app.css";
+    import Nav from "../components/navbar.svelte";
+    import Foot from "../components/footer.svelte";
+  </script>
+  <Nav/>
+  <slot />
+  <Foot/>
---
+++ src/routes/index.svelte
@@ -0,0 +1,96 @@
+<div class="bg-gray-900">
+    <div class="max-w-2xl bg-white mx-auto py-16 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
+      <h1 class="text-6xl text-gray-700 pb-9 font-mono">Sorting Algorithms</h1>
+      <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <a href="#" class="group">
+          <div class="w-full mt-10 aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+            <img src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fcodepumpkin.com%2Fwp-content%2Fuploads%2F2017%2F10%2FBubbleSort_Avg_case.gif&sp=1655649329T0930fb648972855643d5ca5b1ec91aeb7deb9e461fa1e9bd709af03c72f615e3" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75">
+          </div>
+          <h3 class="mt-4 text-2xl text-gray-700">Bubble Sort</h3>
+        </a>
+
+        <a href="#" class="group">
+          <div class="w-full mt-2 aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+            <img src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fwww.dottedsquirrel.com%2Fcontent%2Fimages%2F2021%2F03%2Fselectionsortdemo.gif&sp=1655649395Tb4a7febbc5d1ee3956afb0a76d98300505b1d849ec3d6eba2bcbc486f4cb973e" alt="Olive drab green insulated bottle with flared screw lid and flat top." class="w-full h-full object-center object-cover group-hover:opacity-75">
+          </div>
+          <h3 class="mt-4 text-2xl text-gray-700">Selection Sort</h3>
+        </a>
+
+        <a href="#" class="group">
+          <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+            <img src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F9%2F9c%2FInsertion-sort-example.gif&sp=1655649489T0dafdb69f869524dd83878bcb43b2d10a2d76dc80a39437b48f0eab5c514adcf" alt="Person using a pen to cross a task off a productivity paper card." class="w-full h-full object-center object-cover group-hover:opacity-75">
+          </div>
+          <h3 class="mt-4 text-2xl text-gray-700">Insertion Sort</h3>
+        </a>
+
+        <a href="#" class="group">
+          <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+            <img src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fc%2Fcc%2FMerge-sort-example-300px.gif%3F20151222172210&sp=1655649515Tae60ebec2a23e9ed1c606894f5b5f178e9d32a485292bfca603049c91cf4b106" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="w-full h-full object-center object-cover group-hover:opacity-75">
+          </div>
+          <h3 class="mt-4 text-2xl text-gray-700">Merge Sort</h3>
+        </a>
+
+        <a href="#" class="group">
+            <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+              <img src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fc.tenor.com%2FzswbYsLbYqEAAAAd%2Fcounting-sort.gif&sp=1655642391T5ec1b59a8b6f726b0da62a36459c981e7fac52cf27e8a2cd3e9fb3366035dd48" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="w-full h-full object-center object-cover group-hover:opacity-75">
+            </div>
+            <h3 class="mt-4 text-2xl text-gray-700">Counting Sort</h3>
+          </a>
+
+          <a href="#" class="group">
+            <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+              <img src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fmiro.medium.com%2Fmax%2F800%2F1%2A_2l-UN7jcQp54hgazT6iVA.gif&sp=1655642486T95164f462c3a620062af65011e37e5a1a0699c7794438b6f41040ae4b45594c0" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="w-full h-full object-center object-cover group-hover:opacity-75">
+            </div>
+            <h3 class="mt-4 text-2xl text-gray-700">Bucket Sort</h3>
+          </a>
+
+          <a href="#" class="group">
+            <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+              <img src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F9%2F9c%2FQuicksort-example.gif&sp=1655642621Tbded8eddbfe7e59196766e98a71fb3c9bb1e7503fba13e6ebb2b15f5fcb32c45" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="w-full h-full object-center object-cover group-hover:opacity-75">
+            </div>
+            <h3 class="mt-4 text-2xl text-gray-700">Quick Sort</h3>
+          </a>
+
+          <a href="#" class="group">
+            <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+              <img src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fqph.cf2.quoracdn.net%2Fmain-qimg-a6a90c100e3aa52e07ba2072de7a8c7e&sp=1655642848T5dae61e9d79f143624625bef9f61cd8863790c368824080229eaf38de47ccc44" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="w-full h-full object-center object-cover group-hover:opacity-75">
+            </div>
+            <h3 class="mt-4 text-2xl text-gray-700">Quick Sort</h3>
+          </a>
+        <!-- More products... -->
+      </div>
+    </div>
+    <div class="max-w-2xl bg-white mx-auto py-16 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h1 class="text-6xl pb-9 font-mono">Search Algorithms</h1>
+        <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          <a href="#" class="group">
+            <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+              <img src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fwww.tutorialspoint.com%2Fdata_structures_algorithms%2Fimages%2Flinear_search.gif&sp=1655643462T678c062766d315dcbc52e66cf6f822c8aa3d7516b5856127c92f1fd7d53baa0c" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75">
+            </div>
+            <h3 class="mt-4 text-2xl text-gray-700">Linear Search</h3>
+          </a>
+
+          <a href="#" class="group">
+            <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+              <img src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fd18l82el6cdm1i.cloudfront.net%2Fuploads%2FbePceUMnSG-binary_search_gif.gif&sp=1655643503T55ae27e483ea7d86e67534251d4713af5b3a112546ceba2c160d475227351885" alt="Olive drab green insulated bottle with flared screw lid and flat top." class="w-full h-full object-center object-cover group-hover:opacity-75">
+            </div>
+            <h3 class="mt-4 text-2xl text-gray-700">Binary Search</h3>
+          </a>
+
+          <a href="#" class="group">
+            <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+              <img src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fharkishen-singh.github.io%2Fjump-search-visualisation%2Fthis.gif&sp=1655643567T581b4f2ad73d59d2d040354836e7301a18ab9f25daf320a21c35c6d4a64bd251" alt="Person using a pen to cross a task off a productivity paper card." class="w-full h-full object-center object-cover group-hover:opacity-75">
+            </div>
+            <h3 class="mt-4 text-2xl text-gray-700">Jump Search</h3>
+          </a>
+
+          <a href="#" class="group">
+            <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+              <img src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F0%2F08%2FInterpolation_sort.gif%2F440px-Interpolation_sort.gif&sp=1655643707T956d2acee52df0ac1fc080894a240511945a026589fb08b1a4108f791d738d1b" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="w-full h-full object-center object-cover group-hover:opacity-75">
+            </div>
+            <h3 class="mt-4 text-2xl text-gray-700">Interpolation Search</h3>
+          </a>
+          <!-- More products... -->
+        </div>
+    </div>
+  </div>
