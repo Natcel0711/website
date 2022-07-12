0ae69de2a600e7e8b3eb7c28f7a21db6a35fedee
Natcel <natcelnieves@gmail.com>
2022-07-11 21:14:06 -0400

    Added supabase

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,6 +4,9 @@
     import Foot from "../components/footer.svelte";
     import { ModalOpen } from "../stores/ModalStore";
     import Modal from "../components/modal.svelte";
+    import {supabase} from "../lib/supabase.js";
+
+    console.log(supabase);
   </script>
   {#if $ModalOpen}
     <Modal></Modal>
