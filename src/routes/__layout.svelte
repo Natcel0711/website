fa8e5747110d0badc6fee40bd38f0400e69f89a0
Natcel <natcelnieves@gmail.com>
2022-07-18 10:18:57 -0400

    Get info when app first loads

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,16 +1,16 @@
 <script>
-    import "../app.css";
-    import Nav from "../components/navbar.svelte";
-    import Foot from "../components/footer.svelte";
-    import { ModalOpen, getInfo } from "../stores/ModalStore";
-    import Modal from "../components/modal.svelte";
-    import {supabase} from "../lib/supabase.js";
-    console.log(supabase);
-    getInfo();
-  </script>
-  {#if $ModalOpen}
-    <Modal></Modal>
-  {/if}
-  <Nav/>
-  <slot />
-  <Foot/>
+	import '../app.css';
+	import Nav from '../components/navbar.svelte';
+	import Foot from '../components/footer.svelte';
+	import { ModalOpen } from '../stores/ModalStore';
+	import Modal from '../components/modal.svelte';
+	import { supabase } from '../lib/supabase.js';
+	console.log(supabase);
+</script>
+
+{#if $ModalOpen}
+	<Modal />
+{/if}
+<Nav />
+<slot />
+<Foot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,17 @@
-<svelte:head>
-    <title>Home</title>
-</svelte:head>
 <script>
-  import Homecard from "../components/homecard.svelte";
-  import HomeAbout from "../components/homeabout.svelte"
+	import Homecard from '../components/homecard.svelte';
+	import HomeAbout from '../components/homeabout.svelte';
+	import { getInfo } from '../stores/ModalStore';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		getInfo();
+	});
 </script>
-<Homecard/>
-  <!-- This example requires Tailwind CSS v2.0+ -->
-<HomeAbout/>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+<Homecard />
+<!-- This example requires Tailwind CSS v2.0+ -->
+<HomeAbout />
