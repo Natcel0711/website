31eeac31499659de6dd4ce6e2edeaecd9e6721a0
Natcel <natcelnieves@gmail.com>
2022-07-18 09:50:15 -0400

    Next button component

---
+++ src/components/nextButton.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let whereTo;
+</script>
+<div style="margin-left: 34em; margin-right: 34em;" class="rounded-md shadow">
+    <a
+        href={whereTo}
+        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-900 hover:bg-slate-600 md:py-4 md:text-lg md:px-10"
+    >
+        Next
+    </a>
+</div>
--- src/routes/DataStructures.svelte
+++ src/routes/DataStructures.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { getResult } from '../stores/ModalStore';
 	import Card from '../components/card.svelte';
+	import Button from "../components/nextButton.svelte";
 </script>
 
 <svelte:head>
@@ -19,13 +20,6 @@
 			{/each}
 			<!-- More products... -->
 		</div>
-		<div style="margin-left: 34em; margin-right: 34em;" class="rounded-md shadow">
-			<a
-				href="/Algorithms"
-				class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-900 hover:bg-slate-600 md:py-4 md:text-lg md:px-10"
-			>
-				Next
-			</a>
-		</div>
+		<Button whereTo="/Algorithms" />
 	</div>
 </div>
