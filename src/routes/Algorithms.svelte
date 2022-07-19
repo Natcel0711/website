877447f3ab7858194a3cb0ae4b9bb7f5be3e63b8
Natcel <natcelnieves@gmail.com>
2022-07-19 09:06:44 -0400

    Divide sort and search algorithms

--- src/routes/Algorithms.svelte
+++ src/routes/Algorithms.svelte
@@ -8,12 +8,25 @@
 </svelte:head>
 <div class="bg-white">
 	<div class="max-w-2xl bg-white mx-auto py-16 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
-		<h1 class="text-6xl text-gray-700 pb-9 font-mono">Algorithms</h1>
+		<h1 class="text-6xl text-gray-700 pb-9 font-mono">Sort Algorithms</h1>
 		<div
 			class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
 		>
 			{#each $getResult as Algo}
-				{#if Algo.Category === 'Algorithm'}
+				{#if Algo.Category === 'Algorithm' && Algo.Type === 'Sort'}
+					<Card cardInfo={Algo} />
+				{/if}
+			{/each}
+			<!-- More products... -->
+		</div>
+	</div>
+	<div class="max-w-2xl bg-white mx-auto py-16 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
+		<h1 class="text-6xl text-gray-700 pb-9 font-mono">Search Algorithms</h1>
+		<div
+			class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
+		>
+			{#each $getResult as Algo}
+				{#if Algo.Category === 'Algorithm' && Algo.Type === 'Search'}
 					<Card cardInfo={Algo} />
 				{/if}
 			{/each}
